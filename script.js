b60f512997236a1949fa3c28208e41d247c63086
BestDealWithRam <bhagwandasrai133@gmail.com>
2025-08-20 10:45:47 +0530

    Amazon Gadget

---
+++ script.js
@@ -0,0 +1,7 @@
+alert= 'BestDealHuntWithRam'
+// Example: Simple alert when user clicks Buy Now
+document.querySelectorAll(".btn").forEach(button => {
+  button.addEventListener("click", () => {
+    alert("Redirecting you to Amazon via BestDealHuntWithRam affiliate link!");
+  });
+});
